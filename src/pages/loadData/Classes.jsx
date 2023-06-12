2b2e04b36b9211a570faed99c28db70bebbd82f8
rabiulislam13579 <rabiul120158@gmail.com>
2023-06-12 23:07:44 +0600

    load data

---
+++ src/pages/loadData/Classes.jsx
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from 'react';
+import Class from '../Class/Class';
+
+const Classes = () => {
+    const [musics, setMusics] = useState([]);
+    useEffect(() => {
+        fetch(`https://summer-school-server-six.vercel.app/classes`)
+            .then(res => res.json())
+            .then(data => setMusics(data))
+    }, [])
+
+
+   
+
+
+    return (
+        <div className='grid md:grid-cols-3'>
+            {
+                musics.map(music => <Class
+                key={music._id}
+                music={music}
+                ></Class> )
+                }
+        </div>
+    );
+};
+
+export default Classes;
---
+++ src/pages/loadData/Instructors.jsx
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from 'react';
+import useTitle from '../../hooks/useTitle';
+
+const Instructors = () => {
+    useTitle('Instructors')
+    const [teachers, setTeachers] = useState([]);
+    useEffect(() => {
+        fetch(`https://summer-school-server-six.vercel.app/instructors`)
+            .then(res => res.json())
+            .then(data => setTeachers(data))
+    }, [])
+    return (
+        <div className='grid md:grid-cols-3'>
+            {
+                teachers.map(instructor =>
+                    <div key={instructor._id}>
+                        <div className="  card w-96 bg-base-100 shadow-xl">
+                            <figure className="px-10 pt-10">
+                                <img src={instructor.image} alt="Shoes" className="rounded-xl" />
+                            </figure>
+                            <div className="card-body items-center text-center">
+                                <h2 className="card-title">{instructor.name}</h2>
+                                <p>{instructor.description}</p>
+                                <p>
+                                    Rating: {instructor.rating}</p>
+                                <div className="card-actions">
+                                    <button className="btn btn-primary">View Details</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                )
+            }
+        </div>
+    );
+};
+
+export default Instructors;
